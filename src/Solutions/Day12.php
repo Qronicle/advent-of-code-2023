ebf47f9c71199d1c8747f08cfc4c4a71ca930b82
Ruud Seberechts <ruud.seberechts@uniweb.eu>
2023-12-12 22:29:36 +0100

    Optimize day 12

--- src/Solutions/Day12.php
+++ src/Solutions/Day12.php
@@ -13,53 +13,23 @@ class Day12 extends AbstractSolution
         $sum = 0;
         foreach ($this->getInputLines() as $line) {
             [$springs, $damagedGroups] = explode(' ', $line);
-            $springs = str_split($springs);
-            $damagedGroups = explode(',', $damagedGroups);
-            $sum += $this->getNumPossibilities($springs, $damagedGroups);
+            $damagedGroups = array_map(fn(string $val) => (int)$val, explode(',', $damagedGroups));
+            $num = $this->getSum($springs, $damagedGroups);
+            $sum += $num;
+            $this->cache = [];
         }
         return $sum;
     }
 
     protected function solvePart2(): string
     {
-        return $this->attempt2();
-        ini_set('memory_limit', '12G');
         $sum = 0;
-        foreach ($this->getInputLines() as $l => $line) {
-            if ($l <= 15) continue;
-            [$springs, $damagedGroups] = explode(' ', $line);
-            if (false) {
-                $springs = "$springs?$springs?$springs?$springs?$springs";
-                $damagedGroups = "$damagedGroups,$damagedGroups,$damagedGroups,$damagedGroups,$damagedGroups";
-                $num = $this->getNumPossibilities(str_split($springs), explode(',', $damagedGroups));
-            } else {
-                $num1 = $this->getNumPossibilities(str_split($springs), explode(',', $damagedGroups));
-                $numX = $this->getNumPossibilities(str_split("$springs?$springs"), explode(',', "$damagedGroups,$damagedGroups"));
-                $num2 = $numX / $num1;
-                if (str_contains((string)$num2, '.')) {
-                    $springs = "$springs?$springs?$springs?$springs?$springs";
-                    $damagedGroups = "$damagedGroups,$damagedGroups,$damagedGroups,$damagedGroups,$damagedGroups";
-                    $num = $this->getNumPossibilities(str_split($springs), explode(',', $damagedGroups));
-                } else {
-                    $num = $num1 * $num2 * $num2 * $num2 * $num2;
-                }
-            }
-            $sum += $num;
-            echo "$l. $num ($sum)\n";
-        }
-        return $sum;
-    }
-
-    protected function attempt2(): int
-    {
-        $sum = 0;
-        foreach ($this->getInputLines() as $l => $line) {
+        foreach ($this->getInputLines() as $line) {
             [$springs, $damagedGroups] = explode(' ', $line);
             $springs = "$springs?$springs?$springs?$springs?$springs";
             $damagedGroups = "$damagedGroups,$damagedGroups,$damagedGroups,$damagedGroups,$damagedGroups";
-            $damagedGroups = array_map(fn (string $val) => (int)$val, explode(',', $damagedGroups));
+            $damagedGroups = array_map(fn(string $val) => (int)$val, explode(',', $damagedGroups));
             $num = $this->getSum($springs, $damagedGroups);
-            echo "$l. $num ($sum)\n";
             $sum += $num;
             $this->cache = [];
         }
@@ -68,6 +38,8 @@ class Day12 extends AbstractSolution
 
     protected function getSum(string $springs, array $damagedGroups, int $springIndex = 0, int $groupIndex = 0, int $numDamaged = 0): int
     {
+        // We will encounter the same conditions many a time when running through all permutations
+        // Better cache them!
         if (isset($this->cache[$springIndex][$groupIndex][$numDamaged])) {
             return $this->cache[$springIndex][$groupIndex][$numDamaged];
         }
@@ -75,97 +47,36 @@ class Day12 extends AbstractSolution
         // Last spring calculations!
         if ($springIndex === strlen($springs)) {
             if ($groupIndex === count($damagedGroups) && $numDamaged === 0) {
+                // We already reached the required damaged groups and have no running damaged spring streak
                 return 1;
             } elseif ($groupIndex === count($damagedGroups) - 1 && $damagedGroups[$groupIndex] === $numDamaged) {
+                // We end with a damaged group that matches the final required damaged group length
                 return 1;
-            } else {
-                return 0;
             }
+            // Not a valid permutation!
+            return 0;
         }
 
         $sum = 0;
-        foreach (['.', '#'] as $spring) {
-            if ($springs[$springIndex] === $spring || $springs[$springIndex] === '?') {
-                if ($spring === '.' && $numDamaged === 0) {
-                    $sum += $this->getSum($springs, $damagedGroups, $springIndex + 1, $groupIndex);
-                } elseif ($spring === '.' && $numDamaged > 0 && $groupIndex < count($damagedGroups) && $damagedGroups[$groupIndex] === $numDamaged) {
-                    $sum += $this->getSum($springs, $damagedGroups, $springIndex + 1, $groupIndex + 1);
-                } elseif ($spring === '#') {
-                    $sum += $this->getSum($springs, $damagedGroups, $springIndex + 1, $groupIndex, $numDamaged + 1);
-                }
+        $srcSpring = $springs[$springIndex];
+        // If the damage status of the current spring is unknown, permutate the path to include both options!
+        $targetSprings = $srcSpring === '?' ? ['.', '#'] : [$srcSpring];
+        foreach ($targetSprings as $targetSpring) {
+            if ($targetSpring === '.' && $numDamaged === 0) {
+                // We are not in a damaged spring streak, continue business as usual at the next spring
+                $sum += $this->getSum($springs, $damagedGroups, $springIndex + 1, $groupIndex);
+            } elseif ($targetSpring === '.' && $numDamaged > 0 && ($damagedGroups[$groupIndex] ?? -1) === $numDamaged) {
+                // We are in a damaged spring streak, and the number of damaged springs matches our current group
+                $sum += $this->getSum($springs, $damagedGroups, $springIndex + 1, $groupIndex + 1);
+            } elseif ($targetSpring === '#') {
+                // We grow the damaged spring streak
+                $sum += $this->getSum($springs, $damagedGroups, $springIndex + 1, $groupIndex, $numDamaged + 1);
             }
+            // All other situations indicate that the required damaged groups cannot be formed anymore
         }
+
+        // Cache and return the result son
         $this->cache[$springIndex][$groupIndex][$numDamaged] = $sum;
         return $sum;
-    }
-
-    protected function getNumPossibilities(array $springs, array $damagedGroups): int
-    {
-        $sum = 0;
-        $possibilities = [''];
-        foreach ($springs as $spring) {
-            $newPossibilities = [];
-            if ($spring === '?') {
-                foreach ($possibilities as $possibility) {
-                    $newPossibilities[] = $possibility . '.';
-                    $newPossibilities[] = $possibility . '#';
-                }
-            } else {
-                foreach ($possibilities as $possibility) {
-                    $newPossibilities[] = $possibility . $spring;
-                }
-            }
-            $possibilities = [];
-            foreach ($newPossibilities as $possibility) {
-                if ($this->canHaveValidDamagedGroups($possibility, $damagedGroups)) {
-                    $possibilities[] = $possibility;
-                }
-            }
-        }
-        foreach ($possibilities as $possibility) {
-            if ($this->hasValidDamagedGroups($possibility, $damagedGroups)) {
-                $sum++;
-            }
-        }
-        return $sum;
-    }
-
-    protected function hasValidDamagedGroups(string $springs, array $damagedGroups): bool
-    {
-        $inGroup = false;
-        $numInGroup = 0;
-        $springs .= '.';
-        foreach (str_split($springs) as $spring) {
-            if ($spring === '#') {
-                $inGroup = true;
-                $numInGroup++;
-            } elseif ($inGroup) {
-                if (!$damagedGroups || array_shift($damagedGroups) != $numInGroup) {
-                    return false;
-                }
-                $inGroup = false;
-                $numInGroup = 0;
-            }
-        }
-        return !$damagedGroups;
-    }
-
-    protected function canHaveValidDamagedGroups(string $springs, array $damagedGroups): bool
-    {
-        $inGroup = false;
-        $numInGroup = 0;
-        foreach (str_split($springs) as $spring) {
-            if ($spring === '#') {
-                $inGroup = true;
-                $numInGroup++;
-            } elseif ($inGroup) {
-                if (!$damagedGroups || array_shift($damagedGroups) != $numInGroup) {
-                    return false;
-                }
-                $inGroup = false;
-                $numInGroup = 0;
-            }
-        }
-        return true;
     }
 }
